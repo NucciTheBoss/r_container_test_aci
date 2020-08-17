8479b45cbbde6cccaae2eaec1f1c51d6b4e86170
NucciTheBoss <nucci.programming@gmail.com>
2020-08-17 13:15:38 -0400

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -9,14 +9,15 @@ echo "Setting up executables"
 if [ ! -d ${HOME}/work/sw/r_rstudio ]; then
     mkdir -p ${HOME}/work/sw/r_rstudio
 fi
-`which singularity` pull --dir=${PWD}/bin library://nucci/default/rstudiobase:test
+`which singularity` pull library://nucci/default/rstudiobase:test
+mv rstudiobase_test.sif ${HOME}/work/sw/r_rstudio
 
 # Set up module file
 echo "Setting up module"
 if [ ! -d ${HOME}/work/sw/modules/r_rstudio ]; then
     mkdir -p ${HOME}/work/sw/modules/r_rstudio
 fi
-cp ${PWD}/src/test.lua ${HOME}/sw/modules/r_rstudio/test.lua
+cp ${PWD}/src/test.lua ${HOME}/work/sw/modules/r_rstudio/test.lua
 
 echo "To load the r_rstudio module use the following commands:"
 echo -e "\nmodule use ${HOME}/work/sw/modules"
